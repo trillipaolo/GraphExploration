93ab2b6949eb0efd35f3c8f277ca19e131967b38
Paolo Trilli <paolo.trilli@mail.polimi.it>
2019-08-08 19:10:19 +0200

    Generator of ER and RMAT graphs

---
+++ GraphExploration/graphgen.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+
+int main(int argc, char* argv[]) {
+	const int n_nodes = 120000;
+	const int n_edges = 2560000;
+
+	const float a = 0.45;
+	const float b = 0.25;
+	const float c = 0.15;
+ 
+	TInt::Rnd.PutSeed(0); // initialize random seed
+	PUNGraph G;
+	PNGraph Gd;
+
+	// Undirected Erdos - Renyi random graph.
+	printf("Generating ER small graph.\n");
+	G = TSnap::GenRndGnm<PUNGraph>(n_nodes/100, n_edges/100, false);
+	printf("Saving ER small graph.\n");
+	TSnap::SaveEdgeList(G, "ERgraphSmall.txt");
+	printf("Done 1 of 4.\n");
+
+	printf("Generating ER big graph.\n");
+	G = TSnap::GenRndGnm<PUNGraph>(n_nodes, n_edges, false);
+	printf("Saving ER big graph.\n");
+	TSnap::SaveEdgeList(G, "ERgraphBig.txt");
+	printf("Done 2 of 4.\n");
+
+	// Directed RMAT scale-free random graph.
+	printf("Generating RMAT small graph.\n");
+	Gd = TSnap::GenRMat(n_nodes/100, n_edges/100, a, b, c);
+	printf("Saving RMAT small graph.\n");
+	TSnap::SaveEdgeList(G, "RMATgraphSmall.txt");
+	printf("Done 3 of 4.\n");
+
+	printf("Generating RMAT big graph.\n");
+	Gd = TSnap::GenRMat(n_nodes, n_edges, a, b, c);
+	printf("Saving RMAT big graph.\n");
+	TSnap::SaveEdgeList(G, "RMATgraphBig.txt");
+	printf("Done 4 of 4.\n");
+
+	return 0;
+}
