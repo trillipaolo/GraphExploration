1ecf57b7fb5ce5d1df30d7a5bc5ac4ff86da065f
Paolo Trilli <paolo.trilli@mail.polimi.it>
2019-08-11 23:57:39 +0200

    Added basic parallel BFS

--- GraphExploration/main.cpp
+++ GraphExploration/main.cpp
@@ -2,11 +2,17 @@
 #include <queue>
 #include <set>
 #include <chrono>
+#include <thread>
+#include <mutex>
 
 using namespace std;
 using namespace std::chrono;
 
+short int* levelSynchronousSequentialBFSbasic(bool* graph, int nodes);
 short int* levelSynchronousSequentialBFS(bool* graph, int nodes);
+short int* levelSynchronousParallelBFS(bool* graph, int nodes, int n_max_threads);
+void lSPBFSlevel(bool* graph, int nodes, int curr, bool* visited, queue<int> &next, short int* lev, int level);
+void lSPBFSneighbours(int neigh, bool* visited, queue<int> &next, short int* lev, int level);
 void plotLevelTable(short int* lev, int nodes);
 set<int> nextNodesSet(bool* graph, int node, int nodes);
 queue<int> nextNodesQueue(bool* graph, int node, int nodes);
@@ -15,77 +21,109 @@ void printGraph(bool* graph);
 int charsToInt(char chars[], int length);
 
 int nodes = 0;
+mutex mtx;
 
 int main(int argc, char* argv[]) {
-	bool* graph = importGraph("ERgraphSmall.txt", true);
+	int concurentThreadsSupported = thread::hardware_concurrency();
+
+	printf("MAX_THREADS: %d\n", concurentThreadsSupported);
+
+	time_point<steady_clock> start;
+	time_point<steady_clock> stop;
+	milliseconds duration;
+
+	bool* graph = importGraph("RMATgraphMedium.txt", false);
+	short int* lev;
 
 	//printGraph(graph);
 
-	auto start = high_resolution_clock::now();
-	short int* lev = levelSynchronousSequentialBFS(graph, nodes);
-	auto stop = chrono::high_resolution_clock::now();
-	auto duration = duration_cast<milliseconds>(stop - start);
-	printf("Sequential BFS: %.2d us\n", duration);
+	printf("\nStart Sequential BFS normal\n");
+	start = high_resolution_clock::now();
+	lev = levelSynchronousSequentialBFSbasic(graph, nodes);
+	stop = chrono::high_resolution_clock::now();
+	printf("Done Sequential BFS normal\n");
+	duration = duration_cast<milliseconds>(stop - start);
+	printf("Sequential BFS basic: %.2d us\n", duration);
 
+	plotLevelTable(lev, nodes);
+
+	printf("\nStart Sequential BFS optimized\n");
+	start = high_resolution_clock::now();
+	lev = levelSynchronousSequentialBFS(graph, nodes);
+	stop = chrono::high_resolution_clock::now();
+	printf("Done Sequential BFS optimized\n");
+	duration = duration_cast<milliseconds>(stop - start);
+	printf("Sequential BFS optimized: %.2d us\n", duration);
+	
+	plotLevelTable(lev, nodes);
+
+
+	printf("\nStart Parallel BFS optimized\n");
+	start = high_resolution_clock::now();
+	lev = levelSynchronousParallelBFS(graph, nodes, concurentThreadsSupported);
+	stop = chrono::high_resolution_clock::now();
+	printf("Done Parallel BFS optimized\n");
+	duration = duration_cast<milliseconds>(stop - start);
+	printf("Parallel BFS normal: %.2d us\n", duration);
 
 	plotLevelTable(lev, nodes);
 
 	return 0;
 }
 
-//short int* levelSynchronousSequentialBFS(bool* graph, int nodes) {
-//	/*procedure BFS(r:Node)
-//	V = C = 0;; N = root frg.Visited, Current, and Next set
-//	r.lev = level = 0
-//	repeat
-//		C = N
-//		for Node c in C do.in parallel
-//			for Node n in Nbr(c) do.in parallel
-//				if n not in V then
-//					N = N + n; V = V + n
-//					n.lev = level + 1
-//		level++
-//	until N = 0;*/
-//
-//	
-//	set<int> visited;
-//	set<int> current;
-//	set<int> next;
-//	set<int>::iterator itr;
-//	set<int>::iterator itr2;
-//
-//	int level = 0;
-//	short int* lev = new short int[nodes];
-//	for (int i = 0; i < nodes; i++) {
-//		lev[i] = -1;
-//	}
-//
-//	// root is node 0
-//	visited.insert(0);
-//	next.insert(0);
-//	lev[0] = 0;
-//
-//	while (!next.empty()) {
-//		current.clear();
-//		current = next;
-//		next.clear();
-//
-//		for (itr = current.begin(); itr != current.end(); ++itr) {
-//
-//			set<int> neighbours = nextNodesSet(graph, *itr, nodes);
-//			for (itr2 = neighbours.begin(); itr2 != neighbours.end(); ++itr2) {
-//				if (!visited.count(*itr2)) {
-//					next.insert(*itr2);
-//					visited.insert(*itr2);
-//					lev[*itr2] = level + 1;
-//				}
-//			}
-//		}
-//		level++;
-//	}
-//
-//	return lev;
-//}
+short int* levelSynchronousSequentialBFSbasic(bool* graph, int nodes) {
+	/*procedure BFS(r:Node)
+	V = C = 0;; N = root frg.Visited, Current, and Next set
+	r.lev = level = 0
+	repeat
+		C = N
+		for Node c in C do.in parallel
+			for Node n in Nbr(c) do.in parallel
+				if n not in V then
+					N = N + n; V = V + n
+					n.lev = level + 1
+		level++
+	until N = 0;*/
+
+	
+	set<int> visited;
+	set<int> current;
+	set<int> next;
+	set<int>::iterator itr;
+	set<int>::iterator itr2;
+
+	int level = 0;
+	short int* lev = new short int[nodes];
+	for (int i = 0; i < nodes; i++) {
+		lev[i] = -1;
+	}
+
+	// root is node 0
+	visited.insert(0);
+	next.insert(0);
+	lev[0] = 0;
+
+	while (!next.empty()) {
+		current.clear();
+		current = next;
+		next.clear();
+
+		for (itr = current.begin(); itr != current.end(); ++itr) {
+
+			set<int> neighbours = nextNodesSet(graph, *itr, nodes);
+			for (itr2 = neighbours.begin(); itr2 != neighbours.end(); ++itr2) {
+				if (!visited.count(*itr2)) {
+					next.insert(*itr2);
+					visited.insert(*itr2);
+					lev[*itr2] = level + 1;
+				}
+			}
+		}
+		level++;
+	}
+
+	return lev;
+}
 
 short int* levelSynchronousSequentialBFS(bool* graph, int nodes) {
 	/*procedure BFS(r:Node)
@@ -149,6 +187,89 @@ short int* levelSynchronousSequentialBFS(bool* graph, int nodes) {
 	}
 
 	return lev;
+}
+
+short int* levelSynchronousParallelBFS(bool* graph, int nodes, int n_max_threads) {
+
+	bool* visited = new bool[nodes];
+	for (int i = 0; i < nodes; i++) {
+		visited[i] = false;
+	}
+	queue<int> current;
+	queue<int> next;
+	queue<thread> threads;
+
+	int level = 0;
+	short int* lev = new short int[nodes];
+	for (int i = 0; i < nodes; i++) {
+		lev[i] = -1;
+	}
+
+	// root is node 0
+	visited[0] = true;
+	next.push(0);
+	lev[0] = 0;
+
+	int curr;
+	while (!next.empty()) {
+		// current = queue<int>();		clean a queue
+		swap(current, next);
+
+		while (!current.empty()) {
+
+			curr = current.front();
+
+			//thread t(lSPBFSlevel, graph, nodes, curr, visited, &next, lev, level);
+			threads.push(thread(lSPBFSlevel, graph, nodes, curr, visited, ref(next), lev, level));
+
+			current.pop();
+		}
+
+		while (!threads.empty()) {
+			threads.front().join();
+			threads.pop();
+		}
+
+		level++;
+	}
+
+	return lev;
+}
+
+void lSPBFSlevel(bool* graph, int nodes, int curr, bool* visited, queue<int> &next, short int* lev, int level) {
+	queue<int> neighbours;
+	queue<thread> threads;
+
+	int neigh;
+
+	neighbours = nextNodesQueue(graph, curr, nodes);
+	while (!neighbours.empty()) {
+
+		neigh = neighbours.front();
+
+		if (!visited[neigh]) {
+			//thread t(lSPBFSneighbours, neigh, visited, &next, lev, level);
+			threads.push(thread(lSPBFSneighbours, neigh, visited, ref(next), lev, level));
+		}
+
+		neighbours.pop();
+	}
+
+	while (!threads.empty()) {
+		threads.front().join();
+		threads.pop();
+	}
+}
+
+void lSPBFSneighbours(int neigh, bool* visited, queue<int> &next, short int* lev, int level) {
+	if (!visited[neigh]) {
+		visited[neigh] = true;
+		lev[neigh] = level + 1;
+
+		mtx.lock();
+		next.push(neigh);
+		mtx.unlock();
+	}
 }
 
 void plotLevelTable(short int* lev, int nodes) {
--- GraphGenerator/graphgen.cpp
+++ GraphGenerator/graphgen.cpp
@@ -19,29 +19,42 @@ void generateGraphs(){
 	PUNGraph G;
 	PNGraph Gd;
 
-	// Undirected Erdos - Renyi random graph.
-	printf("Generating ER small graph.\n");
-	G = TSnap::GenRndGnm<PUNGraph>(n_nodes/100, n_edges/100, false);
-	printf("Saving ER small graph.\n");
-	TSnap::SaveEdgeList(G, "ERgraphSmall.txt");
-	printf("Done 1 of 4.\n");
+	//// Undirected Erdos - Renyi random graph.
+	//printf("Generating ER small graph.\n");
+	//G = TSnap::GenRndGnm<PUNGraph>(n_nodes/100, n_edges/100, false);
+	//printf("Saving ER small graph.\n");
+	//TSnap::SaveEdgeList(G, "ERgraphSmall.txt");
+	//printf("Done 1 of 4.\n");
+
+	//printf("Generating ER big graph.\n");
+	//G = TSnap::GenRndGnm<PUNGraph>(n_nodes, n_edges, false);
+	//printf("Saving ER big graph.\n");
+	//TSnap::SaveEdgeList(G, "ERgraphBig.txt");
+	//printf("Done 2 of 4.\n");
+
+	//// Directed RMAT scale-free random graph.
+	//printf("Generating RMAT small graph.\n");
+	//Gd = TSnap::GenRMat(n_nodes/100, n_edges/100, a, b, c);
+	//printf("Saving RMAT small graph.\n");
+	//TSnap::SaveEdgeList(Gd, "RMATgraphSmall.txt");
+	//printf("Done 3 of 4.\n");
+
+	//printf("Generating RMAT big graph.\n");
+	//Gd = TSnap::GenRMat(n_nodes, n_edges, a, b, c);
+	//printf("Saving RMAT big graph.\n");
+	//TSnap::SaveEdgeList(Gd, "RMATgraphBig.txt");
+	//printf("Done 4 of 4.\n");
 
 	printf("Generating ER big graph.\n");
-	G = TSnap::GenRndGnm<PUNGraph>(n_nodes, n_edges, false);
+	G = TSnap::GenRndGnm<PUNGraph>(n_nodes / 10, n_edges / 10, false);
 	printf("Saving ER big graph.\n");
-	TSnap::SaveEdgeList(G, "ERgraphBig.txt");
+	TSnap::SaveEdgeList(G, "ERgraphMedium.txt");
 	printf("Done 2 of 4.\n");
 
 	// Directed RMAT scale-free random graph.
 	printf("Generating RMAT small graph.\n");
-	Gd = TSnap::GenRMat(n_nodes/100, n_edges/100, a, b, c);
+	Gd = TSnap::GenRMat(n_nodes / 10, n_edges / 10, a, b, c);
 	printf("Saving RMAT small graph.\n");
-	TSnap::SaveEdgeList(Gd, "RMATgraphSmall.txt");
+	TSnap::SaveEdgeList(Gd, "RMATgraphMedium.txt");
 	printf("Done 3 of 4.\n");
-
-	printf("Generating RMAT big graph.\n");
-	Gd = TSnap::GenRMat(n_nodes, n_edges, a, b, c);
-	printf("Saving RMAT big graph.\n");
-	TSnap::SaveEdgeList(Gd, "RMATgraphBig.txt");
-	printf("Done 4 of 4.\n");
 }
